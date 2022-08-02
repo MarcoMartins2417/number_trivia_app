cab33e3dc634e320b40e4fc824c5283950d5da73
Marco Martins <marco.martins@exaud.com>
2022-08-02 16:18:47 +0100

    7 - SetUp for the Network Connection Test

---
+++ number_trivia_app/test/core/network/network_info_test.dart
@@ -0,0 +1,16 @@
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:number_trivia_app/core/network/network_info.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+}
