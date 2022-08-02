ba58d6971bc5f1b31c99ce72faaee0cd8a1dda07
Marco Martins <marco.martins@exaud.com>
2022-08-02 11:09:13 +0100

    3 - Fixing Imports

--- number_trivia_app/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ number_trivia_app/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:number_trivia_app/core/error/failures.dart';
-import 'package:number_trivia_app/core/usecases/usecase.dart';
-import 'package:number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
@@ -15,9 +15,4 @@ class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
     return await repository.getRandomNumberTrivia();
   }
 
-}
-
-class NoParams extends Equatable {
-  @override
-  List<Object> get props => throw UnimplementedError();
 }
