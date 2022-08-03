da1067c26437eda929fbcc0c7928972fc6c3ecf2
Marco Martins <marco.martins@exaud.com>
2022-08-03 16:11:11 +0100

    9 - Created setUp for the Remote DataSource Test

---
+++ number_trivia_app/test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia_app/core/error/exceptions.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:matcher/matcher.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  NumberTriviaRemoteDataSourceImpl dataSource;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  })
+}
