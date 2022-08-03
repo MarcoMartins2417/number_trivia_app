b5b28b101b5290829903559112b194cdfc640231
Marco Martins <marco.martins@exaud.com>
2022-08-03 16:25:41 +0100

    9 - Created Test for GET the Number Trivia with Remote DataSource

--- number_trivia_app/test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
+++ number_trivia_app/test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia_app/core/error/exceptions.dart';
-import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:matcher/matcher.dart';
 import 'package:http/http.dart' as http;
@@ -19,5 +19,22 @@ void main() {
   setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
-  })
+  });
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+
+    test(
+      'should perform a GET request on a URL with a number being the endpoint and with application/json header',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers')))
+            .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+        // act
+        dataSource.getConcreteNumberTrivia(tNumber);
+        // assert
+        verify(mockHttpClient.get('http://numbersapi.com/$tNumber', headers: {'Content-Type': 'application/json'}));
+      },
+    );
+  });
 }
