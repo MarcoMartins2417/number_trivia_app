fcd8f8f257c3ad048764b127bf7ea2016fb27cd6
Marco Martins <marco.martins@exaud.com>
2022-08-04 12:37:22 +0100

    5 - Fixed Path style

--- number_trivia_app/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ number_trivia_app/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,4 +1,4 @@
-import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import '../../data/models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
