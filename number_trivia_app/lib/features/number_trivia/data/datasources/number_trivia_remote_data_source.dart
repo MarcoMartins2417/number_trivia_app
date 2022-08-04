fbf1bd2106276ec8d9ddf410e833e0fc23088002
Marco Martins <marco.martins@exaud.com>
2022-08-04 10:34:49 +0100

    9 - Implemented Test to GET the Number Trivia through Remote Data with the creation of the Remote DataSource File

---
+++ number_trivia_app/lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) {
+    return null;
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    return null;
+  }
+
+}
