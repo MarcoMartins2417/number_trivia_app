267c81825cf3de8d6bb694afa0ecd0045a1ec4cf
Marco Martins <marco.martins@exaud.com>
2022-08-05 11:51:08 +0100

    10 - Created classes for possible states of Number Trivia

--- number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,27 @@
-part of 'number_trivia_bloc.dart';
+import 'dart:html';
+
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaState extends Equatable {
-  const NumberTriviaState();
-  
   @override
   List<Object> get props => [];
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia});
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message});
+}
