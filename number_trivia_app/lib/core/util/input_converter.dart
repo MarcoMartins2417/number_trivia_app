40ba8197f88833bbd30f6ea0a8c648b872e9170c
Marco Martins <marco.martins@exaud.com>
2022-08-05 11:27:29 +0100

    10 - Renamed file

---
+++ number_trivia_app/lib/core/util/input_converter.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import '../error/failures.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+
+  }
+}
+
+class InvalidInputFailure extends Failure {}
