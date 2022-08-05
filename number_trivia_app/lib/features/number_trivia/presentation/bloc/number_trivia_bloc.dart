ed19a72429014added17f2be34bfe2e092341d70
Marco Martins <marco.martins@exaud.com>
2022-08-05 14:39:32 +0100

    11 - Fixed Number Trivia bLoc

--- number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,13 +1,27 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
-part 'number_trivia_event.dart';
-part 'number_trivia_state.dart';
+import '../../../../core/util/input_converter.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
+import 'bloc.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required GetConcreteNumberTrivia concrete,
+    @required GetRandomNumberTrivia random,
+    @required this.inputConverter,
+  })  : assert(concrete != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
+  @override
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) async* {}
 }
