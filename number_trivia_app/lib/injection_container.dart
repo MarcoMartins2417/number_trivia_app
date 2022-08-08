97a7c230a3f599806eec41a189096a0037c2148f
Marco Martins <marco.martins@exaud.com>
2022-08-08 15:07:24 +0100

    13 - Created Features registration

--- number_trivia_app/lib/injection_container.dart
+++ number_trivia_app/lib/injection_container.dart
@@ -1,11 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:number_trivia_app/core/network/network_info.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+import 'core/util/input_converter.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/bloc.dart';
 
 final sl = GetIt.instance;
 
 void init() {
-  // Features - Number Trivia - Registracion 
+  // Features - Number Trivia - Registration
+  sl.registerFactory((() => NumberTriviaBloc(concrete: sl(), random: sl(), inputConverter: sl()))); // Factory - Always instanciate a new thing from the class
+
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));  // Cache and give out the same instance without instanciate a new one
+  sl.registerLazySingleton<NumberTriviaRepository>(() => NumberTriviaRepositoryImpl(remoteDataSource: sl(), localDataSource: sl(), networkInfo: sl()));
+
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(() => NumberTriviaRemoteDataSourceImpl(client: sl()));
+
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(() => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
 
   // Core stuff
+
 
   // External stuff
 }
