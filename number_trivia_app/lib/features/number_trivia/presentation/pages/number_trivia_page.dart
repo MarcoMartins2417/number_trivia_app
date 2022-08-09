3d4dc700971eb162d76be7635f6c80fbbbe71a6e
Marco Martins <marco.martins@exaud.com>
2022-08-09 11:53:04 +0100

    14 - Build body of the page for the app

--- number_trivia_app/lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ number_trivia_app/lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,9 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia_app/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+import '../../../../injection_container.dart';
 
 class NumberTriviaPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text('Number Trivia')),);
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Number Trivia')), body: buildBody(context));
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: Placeholder(),
+              ),
+              SizedBox(height: 20),
+              Column(
+                children: <Widget>[
+                  Placeholder(fallbackHeight: 40),
+                  SizedBox(height: 10),
+                  Row(children: <Widget>[
+                    Expanded(
+                      child: Placeholder(fallbackHeight: 30),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Placeholder(fallbackHeight: 30),
+                    ),
+                  ])
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
