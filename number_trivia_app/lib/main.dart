a1d4a405680082a0151d59d0843a5ffa9c1a7800
Marco Martins <marco.martins@exaud.com>
2022-08-09 15:29:11 +0100

    14 - Fixed format of the code

--- number_trivia_app/lib/main.dart
+++ number_trivia_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:number_trivia_app/features/number_trivia/presentation/pages/number_trivia_page.dart';
+
+import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -11,12 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Number Trivia',
-      theme: ThemeData(
-        primaryColor: Colors.green.shade800,
-        accentColor: Colors.green.shade600,
-      ),
-      home: NumberTriviaPage()
-    );
+        title: 'Number Trivia',
+        theme: ThemeData(
+          primaryColor: Colors.green.shade800,
+          accentColor: Colors.green.shade600,
+        ),
+        home: NumberTriviaPage());
   }
 }
